39430a1725e5cf8920a7cacaf39439794ff49a39
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2019-04-21 15:59:10 -0500

    Dark theme fixes

--- assets/properties.js
+++ assets/properties.js
@@ -2,73 +2,76 @@ var blankDarkTheme = '../wp-content/plugins/bits-custom-admin-theme/assets/dark-
 var darkTheme = '../wp-content/plugins/bits-custom-admin-theme/assets/dark-theme/dark-theme.css';
 var tinyColorGit = 'https://cdn.rawgit.com/bgrins/TinyColor/master/tinycolor.js';
 
-function preloadFunc(){
-jQuery.getJSON("/wp-content/plugins/bits-custom-admin-theme/settings/options.json", function(json) {
-	//console.log(json); // this will show the info it in firebug console   
-	function getValue(key, json){
-		var value = json[key];
-		return value;
-	}
-
-	if(getValue("theme", json) == "dark"){
-		changeCSS(darkTheme, blankDarkTheme);
-		console.log("Set theme to dark");
-	} else {
-		changeCSS(blankDarkTheme, darkTheme);
-		console.log("Set theme to default");
-	}
-	var colors = getValue("colors", json);
-
-	function changeCSS(cssFile, cssLinkIndex) {
-    		var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-
-    		var newlink = document.createElement("link");
-    		newlink.setAttribute("rel", "stylesheet");
-    		newlink.setAttribute("type", "text/css");
-    		newlink.setAttribute("href", cssFile);
-
-    		document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-	}
-	var html = document.getElementsByTagName('html')[0];
-	function changePrimaryColor(){
-		var hexcolor = getValue("primary", colors);
-		html.style.setProperty("--primary-color", hexcolor);
-		console.log("Primary color set to "+hexcolor);
-    		//document.querySelector('meta[name="theme-color"]').setAttribute('content', hexcolor);
-		findColorForText("primary", hexcolor);
-	}
-	function changeAccentColor(){
-		var hexcolor = getValue("accent", colors);
-		html.style.setProperty("--accent-color", hexcolor);
-		console.log("Accent color set to "+hexcolor);
-		findColorForText("accent", hexcolor);
-	}
-	function findColorForText(name, hexcolor){
-		jQuery.getScript(tinyColorGit, function(){
-			var color = tinycolor(hexcolor);
-			if (color.isLight()){
-				html.style.setProperty("--"+name+"-color-text", "#000");
-				console.log("Accent text color set to #000");
-			} else if (color.isDark()){
-				html.style.setProperty("--"+name+"-color-text", "#fff");
-				console.log("Accent text color set to #fff");
-			}
-		});
-		
-	}
-	
-	function changeFontType(){
-		var selectedFont = getValue("font", json);
-		if (selectedFont == 'roboto'){
-			html.style.setProperty("--font-type", "Roboto");
-		} else {
-			html.style.setProperty("--font-type", "unset");
+function preloadFunc() {
+	jQuery.getJSON("/wp-content/plugins/bits-custom-admin-theme/settings/options.json", function (json) {
+		//console.log(json); // this will show the info it in firebug console   
+		function getValue(key, json) {
+			var value = json[key];
+			return value;
 		}
-	}
-	changePrimaryColor();
-	changeAccentColor();
-	changeFontType();
-});
+
+		if (getValue("theme", json) == "dark") {
+			changeCSS(darkTheme, blankDarkTheme);
+			console.log("Set theme to dark");
+		} else {
+			changeCSS(blankDarkTheme, darkTheme);
+			console.log("Set theme to default");
+		}
+		var colors = getValue("colors", json);
+
+		function changeCSS(cssFile, cssLinkIndex) {
+			var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+			var newlink = document.createElement("link");
+			newlink.setAttribute("rel", "stylesheet");
+			newlink.setAttribute("type", "text/css");
+			newlink.setAttribute("href", cssFile);
+
+			oldlink.replaceWith(newlink);
+			// document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+		}
+		var html = document.getElementsByTagName('html')[0];
+		function changePrimaryColor() {
+			var hexcolor = getValue("primary", colors);
+			html.style.setProperty("--primary-color", hexcolor);
+			console.log("Primary color set to " + hexcolor);
+			//document.querySelector('meta[name="theme-color"]').setAttribute('content', hexcolor);
+			findColorForText("primary", hexcolor);
+		}
+		function changeAccentColor() {
+			var hexcolor = getValue("accent", colors);
+			html.style.setProperty("--accent-color", hexcolor);
+			console.log("Accent color set to " + hexcolor);
+			findColorForText("accent", hexcolor);
+		}
+		function findColorForText(name, hexcolor) {
+			jQuery.getScript(tinyColorGit, function () {
+				var color = tinycolor(hexcolor);
+				if (color.isLight()) {
+					html.style.setProperty("--" + name + "-color-text", "#000");
+					console.log("Accent text color set to #000");
+				} else if (color.isDark()) {
+					html.style.setProperty("--" + name + "-color-text", "#fff");
+					console.log("Accent text color set to #fff");
+				}
+			});
+
+		}
+
+		function changeFontType() {
+			var selectedFont = getValue("font", json);
+			if (selectedFont == 'roboto') {
+				html.style.setProperty("--font-type", "Roboto");
+			} else if (selectedFont == 'productsans') {
+				html.style.setProperty("--font-type", "Product Sans");
+			} else {
+				html.style.setProperty("--font-type", "unset");
+			}
+		}
+		changePrimaryColor();
+		changeAccentColor();
+		changeFontType();
+	});
 }
 window.onpaint = preloadFunc();
 
