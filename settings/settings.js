8cdfbea6d0f65ede63e40dd73dc093caf19920d3
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2019-04-23 03:22:42 -0500

    - Safari dark mode - Dark mode visual changes

--- settings/settings.js
+++ settings/settings.js
@@ -70,5 +70,5 @@ function changeCSS(cssFile, cssLinkIndex) {
 	newlink.setAttribute("type", "text/css");
 	newlink.setAttribute("href", cssFile);
 
-	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+	oldlink.replaceWith(newlink);
 }
